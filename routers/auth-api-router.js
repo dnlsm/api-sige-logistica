f68df9c6e85c6ef5c69f0244ca323d26e7041b6c
Danilo Soares Mendes <dnlsm@bk.ru>
2018-08-29 14:53:51 -0300

    Implementing /api/auth route

---
+++ routers/auth-api-router.js
@@ -0,0 +1,56 @@
+const express = require('express')
+var router = express.Router()
+
+const {SELECT, INSERT_INTO} = require('../utils/db-connect')
+const {INVALID_CREDENTIALS,MISSING_PARAMETERS,INTERNAL_SERVER_ERROR} = require('../utils/error-messages')
+const {generateToken} = require('../utils/security')
+
+require('../utils/date-manipulation')
+
+
+router.get('/', (req,res)=>{
+	// Obtem os parâmetros ad url ?usr=&pwd=
+	var username = req.query.usr
+	var password = req.query.pwd
+
+	// res.json retorna um json para ao usuário
+	if(!username || !password)
+		res.json(MISSING_PARAMETERS)
+	else  // SELECT * FROM USER WHERE user_login = "username" AND user_password = "password"
+		SELECT('*', 'USER', [
+								["user_login", username]
+								["user_password", password]
+							]
+		)
+		.exec()
+		.onOne((result)=>{
+				// new Date() consulta o timestamp atual
+				// shift adiciona ou subtrai um determinado tempo
+				var creation = new Date()
+				var expiration = creation.shift(3000000)
+
+				var token = generateToken(255)
+				// INSERT INTO SESSION SET session_token = token, session_is_active = 1 ......
+				INSERT_INTO('SESSION',	[
+											['session_token', token],
+											['session_is_active', 1],
+											['session_creation', creation.toMySQL()],
+											['session_expiration', expiration.toMySQL()],
+											['fk_session_user_login', username]
+										]
+							)
+				.exec()
+				.then((results, fields)=>{
+					res.json(	{
+									msg	: "Login successful",
+									status_code	: 200,
+									return	:	{ token, creation, expiration}
+								}
+							)
+				})
+				.error(()=> res.json(INTERNAL_SERVER_ERROR))
+		})
+		.onZero(()=> res.json(INVALID_CREDENTIALS))
+})
+
+module.exports = router
