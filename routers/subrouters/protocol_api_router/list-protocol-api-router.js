b31d1044c15cfe930afa580856859e5ad482e8c3
Danilo Soares Mendes <dnlsm@bk.ru>
2018-08-30 15:27:36 -0300

    implementation /api/inner/protocol/list

--- routers/subrouters/protocol_api_router/list-protocol-api-router.js
+++ routers/subrouters/protocol_api_router/list-protocol-api-router.js
@@ -3,9 +3,37 @@
 const express = require('express')
 const router = express.Router()
 
+// importando utilitários
+const {SELECT} = require('../../../utils/db-connect')
+const {PROTOCOL_NOT_FOUND,MISSING_PARAMETERS,INTERNAL_SERVER_ERROR} = require('../../../utils/error-messages')
 
 router.get('/', (req,res)=>{
-	res.end('list-protocol-api-router')
+	SELECT('*', 'PROTOCOL')
+	.exec()
+	.onAny((results)=>{
+		res.json(	{
+						msg	: "Protocol found",
+						status_code	: 200,
+						return	:	results.map((el)=>
+										{
+											type: 			"PROTOCOL",
+											code: 			el['protocol_code'],
+											name: 			el['protocol_name'],
+											client: 		el['protocol_client'],
+											glab_code: 		el['protocol_glab_code'],
+											sq_code: 		el['protocol_sq_code'],
+											cpqdic_code: 	el['protocol_cpqdic_code'],
+											photos_url: 	el['protocol_photos_url'],
+											invoice: 		el['protocol_invoice'],
+											invoice_folder: el['protocol_invoice_folder'],
+											notes: 			el['protocol_notes']
+										}
+									)
+					}
+			)
+	})
+	.onZero(()=>res.json(PROTOCOL_NOT_FOUND))
+	.error(()=>res.json(INTERNAL_SERVER_ERROR))
 })
 
 
