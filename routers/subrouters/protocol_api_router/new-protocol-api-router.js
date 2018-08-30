ac45236613ac0c51916343872a2e48b2caff8d6b
Danilo Soares Mendes <dnlsm@bk.ru>
2018-08-30 16:57:46 -0300

    waiting a #TODO

--- routers/subrouters/protocol_api_router/new-protocol-api-router.js
+++ routers/subrouters/protocol_api_router/new-protocol-api-router.js
@@ -3,9 +3,30 @@
 const express = require('express')
 const router = express.Router()
 
+const {SELECT} = require('../../../utils/db-connect')
+const {MISSING_PARAMETERS,INTERNAL_SERVER_ERROR} = require('../../../utils/error-messages')
+
 
 router.get('/', (req,res)=>{
-	res.end('new-protocol-api-router')
+	var code 			= req.query.code
+	var name 			= req.query.name
+	var client 			= req.query.client
+	var glab_code 		= req.query.glab_code
+	var sq_code 		= req.query.sq_code
+	var cpqdic_code 	= req.query.cpqdic_code
+	var photos_url 		= req.query.photos_url
+	var invoice 		= req.query.invoice
+	var invoice_folder 	= req.query.invoice_folder
+	var notes 			= req.query.notes
+
+	if(!code || !name || !client)
+		return res.json(MISSING_PARAMETERS)
+
+	SELECT('*', 'PROTOCOL', [['protocol_code',code]])
+	.exec()
+	.onZero(/*#TODO*/)
+	.onAny(/*#TODO*/)
+	.error(()=>req.json(INTERNAL_SERVER_ERROR))
 })
 
 
