4815f443fc184a2c5c87971f07fc5878b59c628f
Rafael Soares Matos <rafael.soaresmat@gmail.com>
2018-08-31 15:07:21 -0300

    update do new-item-api-router

--- routers/subrouters/item_api_router/new-item-api-router.js
+++ routers/subrouters/item_api_router/new-item-api-router.js
@@ -5,10 +5,10 @@ const router = express.Router()
 
 // importando utilitários
 const {SELECT, INSERT_INTO} = require('../../../utils/db-connect')
-const {MISSING_PARAMETERS,INTERNAL_SERVER_ERROR,ITEM_NOT_FOUND} = require('../../../utils/error-messages')
+const {MISSING_PARAMETERS,INTERNAL_SERVER_ERROR,ITEM_NOT_FOUND,ITEM_ALREADY_EXISTS,PROTOCOL_NOT_FOUND} = require('../../../utils/error-messages')
 
 
-router.post('/', (req,res)=>{
+router.get('/', (req,res)=>{
 
 	var item_code = req.query.item_code
 	var item_name = req.query.item_name
@@ -20,50 +20,53 @@ router.post('/', (req,res)=>{
 		case 'ROOT':
 		case 'LOGISTIC':
 					if(item_code == null || item_name == null || item_protocol == null)
-						return {
-							msg : MISSING_PARAMETERS
-							status_code: 400
-						}
-					else
-							SELECT ('*', 'PROTOCOL', [
+						return res.json(MISSING_PARAMETERS)
+
+					SELECT ('*', 'PROTOCOL', [
 								["protocol_code", item_protocol]
 							])
 							.exec()
 							.onAny((result)=>{
 										result = result[0]
 										SELECT ('*', 'ITEM',[
-											["item_code", itemCode]
+											["item_code", item_code]
 										]
-							)
-							.exec()
-							.onAny((result)=>
-										res.json(ITEM_ALREADY_EXISTS)
-							)
-							.onZero((result)=>
+									)
+									.exec()
+									.onAny((result)=>
+													res.json(ITEM_ALREADY_EXISTS)
+												)
+												.onZero((result)=>
+													INSERT_INTO('ITEM', [
+														['item_code', item_code],
+														['item_name', item_name],
+														['fk_item_protocol_code', item_protocol]
+													])
+													.exec()
+													.then((results, fields)=>{
+															res.json({
+																	msg : 'Item created successfully',
+																	status_code : 200
+														})
+													})
 
-
-						)
-
-
-
-
-
+												)
+												.error(()=> res.json(INTERNAL_SERVER_ERROR))
 							})
 							.onZero(()=>
 								res.json(PROTOCOL_NOT_FOUND)
-						)
-						.error(()=> res.json(INTERNAL_SERVER_ERROR))
-
+							)
+						  .error(()=> res.json(INTERNAL_SERVER_ERROR))
 
 					break;
 		case 'LABORATORY':
 		case 'ADMINISTRATIVE':
-
-
-		break;
+					res.json({
+						msg : "Forbidden",
+						status_code : 403
+					})
+					break;
 	}
-
-
 
 })
 
