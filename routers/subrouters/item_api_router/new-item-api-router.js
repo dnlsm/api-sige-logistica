6417f6474cf716ab176963d09b8d168aa07f9fd2
Rafael Soares Matos <rafael.soaresmat@gmail.com>
2018-08-31 13:26:34 -0300

    update do new-item-api-router

--- routers/subrouters/item_api_router/new-item-api-router.js
+++ routers/subrouters/item_api_router/new-item-api-router.js
@@ -10,7 +10,59 @@ const {MISSING_PARAMETERS,INTERNAL_SERVER_ERROR,ITEM_NOT_FOUND} = require('../..
 
 router.post('/', (req,res)=>{
 
-	var 
+	var item_code = req.query.item_code
+	var item_name = req.query.item_name
+	var item_protocol = req.query.item_protoc
+	var category = req.user_credentials.a_user_category
+
+	switch(category){
+
+		case 'ROOT':
+		case 'LOGISTIC':
+					if(item_code == null || item_name == null || item_protocol == null)
+						return {
+							msg : MISSING_PARAMETERS
+							status_code: 400
+						}
+					else
+							SELECT ('*', 'PROTOCOL', [
+								["protocol_code", item_protocol]
+							])
+							.exec()
+							.onAny((result)=>{
+										result = result[0]
+										SELECT ('*', 'ITEM',[
+											["item_code", itemCode]
+										]
+							)
+							.exec()
+							.onAny((result)=>
+										res.json(ITEM_ALREADY_EXISTS)
+							)
+							.onZero((result)=>
+
+
+						)
+
+
+
+
+
+							})
+							.onZero(()=>
+								res.json(PROTOCOL_NOT_FOUND)
+						)
+						.error(()=> res.json(INTERNAL_SERVER_ERROR))
+
+
+					break;
+		case 'LABORATORY':
+		case 'ADMINISTRATIVE':
+
+
+		break;
+	}
+
 
 
 })
