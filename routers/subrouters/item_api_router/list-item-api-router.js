5826da4b9a0a247d95c5fc97bb829c399af2be1d
Rafael Soares Matos <rafael.soaresmat@gmail.com>
2018-09-03 10:57:39 -0300

    add list item e index do movement

--- routers/subrouters/item_api_router/list-item-api-router.js
+++ routers/subrouters/item_api_router/list-item-api-router.js
@@ -3,9 +3,41 @@
 const express = require('express')
 const router = express.Router()
 
+// importando utilitários
+const {SELECT, INSERT_INTO} = require('../../../utils/db-connect')
+const {MISSING_PARAMETERS,INTERNAL_SERVER_ERROR,ITEM_NOT_FOUND,PROTOCOL_NOT_FOUND,NO_MOVEMENTS_FOUND} = require('../../../utils/error-messages')
+
 
 router.get('/', (req,res)=>{
-	res.end('list-item-api-router')
+
+	var item_code = req.query.item_code
+
+	if(item_code == null){
+			return res.json(MISSING_PARAMETERS)
+	}
+
+	SELECT('*', 'ITEM',[
+			["item_code", item_code]
+	])
+	.exec()
+	.onAny((result)=>{
+			res.json({
+					msg: "Itens found",
+					status_code: 200,
+					return: result.map((el)=>({
+							 						type: 'ITEM',
+													code: el.item_code,
+													name: el.item_name,
+													protocol: el.fk_item_protocol_code
+					}))
+			})
+	})
+	.onZero(()=>
+			res.json(ITEM_NOT_FOUND)
+	)
+	.error(()=> res.json(INTERNAL_SERVER_ERROR))
+
+
 })
 
 
